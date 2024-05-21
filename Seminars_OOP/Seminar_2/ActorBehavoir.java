8042d60a46f44b143e06740bed20bfff848f5a11
MarinLIS <malinalu@mail.ru>
2024-05-21 14:21:41 +0400

    Seminar_2 is added

---
+++ Seminars_OOP/Seminar_2/ActorBehavoir.java
@@ -0,0 +1,16 @@
+﻿package Seminars_OOP.Seminar_2;
+
+/*Интерфейс ActorBehavoir,
+который будет содержать
+описание возможных
+действий актора в
+очереди/магазине */
+public interface ActorBehavoir {
+    void setMakeOrder(boolean makeOrder);
+
+    void setTakeOrder(boolean takeOrder);
+    
+    boolean isMakeOrder();
+
+    boolean isTakeOrder();
+}
---
+++ Seminars_OOP/Seminar_2/MarketBehavoir.java
@@ -0,0 +1,11 @@
+﻿package Seminars_OOP.Seminar_2;
+
+import java.util.List;
+
+public interface MarketBehavoir {
+    void acceptToMarket(Actor actor);// входит в магазин
+
+    void releasedFromMarket(List<Actor> actorlist); //выходит из магазина
+
+    void update();
+}
